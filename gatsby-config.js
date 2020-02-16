06e35db1f1cb3dd1ecaef77ef99a0f8ed8347a78
jimachu <maiyasu8281@gmail.com>
2020-02-17 00:11:38 +0900

    add google anatlyics.

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,7 +51,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-158650529-1`,
       },
     },
     `gatsby-plugin-feed`,
